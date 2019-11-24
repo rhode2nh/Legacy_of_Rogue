4ff97cbb279165c30d66e3347387298ab815b167
rhode2nh <rhode2nh@dukes.jmu.edu>
2019-11-24 17:27:48 -0500

    Removed test print for raycast collision

--- Assets/Entities/Characters/Enemies/Enemy0/Enemy0.gd
+++ Assets/Entities/Characters/Enemies/Enemy0/Enemy0.gd
@@ -3,7 +3,7 @@ extends "res://Assets/Entities/Characters/BaseEnemy/BaseEnemy.gd"
 func _physics_process(delta):
 	if $RayCast2D.is_colliding():
 		if $RayCast2D.get_collider().is_in_group("Player"):
-			print("test")
+			pass
 			
 			
 			
